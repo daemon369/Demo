5d2c4c7ae086d9299a74667d0fdb4ace04b6a3be
daemon369 <yc@yc-pc.(none)>
2013-07-21 19:29:58 +0800

    写了个c++demo练手，准备了py项目文件夹

---
+++ 20130721/myfirst.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main()
+{
+	using namespace std;
+	cout << "come up and C++ me some time.";
+	cout << endl;
+	cout << "you won't regret it!" << endl;
+	int carrots;
+	carrots = 25;
+	cout << "I hava " << carrots << " carrots." << endl;
+	cout << "How many do you have?"<<endl;
+	cin >> carrots;
+	cout << "Now you have " << carrots << " carrots." << endl;
+	return 0;
+}
